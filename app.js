db965cb445d1f0d4cef4ab899c7649ff42f7cc83
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-10-20 18:08:00 +0500

    Basic server
    Basic folder structure and server setup.

---
+++ app.js
@@ -0,0 +1,13 @@
+// const express = require('express')
+// const app = express()
+import express from 'express';
+const app = express();
+let http = require('http').Server(app);
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+http.listen(port, () => {
+  console.log('listening on ', port);
+})
